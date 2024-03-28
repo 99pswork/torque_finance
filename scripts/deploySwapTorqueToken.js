53c3e3f8835ba7542d032cbcb6d75301f38a1b19
PS <99ps.work@gmail.com>
2024-03-29 01:18:28 +0530

    Update name

--- scripts/deploySwapTorqueToken.js
+++ scripts/deploySwapTorqueToken.js
@@ -12,7 +12,7 @@
 
 
 const hre = require("hardhat");
-// npx hardhat verify --network arbitrum 0xea15bd7a9382fA7dcDED328cBBCFDEC6c397c531 "0xC4B853F10f8fFF315F21C6f9d1a1CEa8fbF0Df01"
+// npx hardhat verify --network arbitrum 0xC6d9a406cB7A72381eF3E33A86BeFEc2dbf05b1F "0xC4B853F10f8fFF315F21C6f9d1a1CEa8fbF0Df01"
 async function deploySwapTorqueToken() {
 
   const SwapTorque = await hre.ethers.getContractFactory("SwapTorqueToken");
